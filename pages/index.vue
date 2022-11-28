d261c358fc90c1b4626f689076ab74be94a1dfa7
Frank Corona Prendes <frank.corona@pm.me>
2022-11-28 17:00:35 -0300

    feat: integrate into index page defined components

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <body>
+    <header>
+      <img alt="Doggies logo" class="logo" src="~/assets/logo.svg" />
+      <DoggiesHeader msg="The Doggies Explorer" />
+    </header>
+
+    <TheDoggies />
+  </body>
+</template>
+
+<script>
+import TheDoggies from '../components/Doggies.vue'
+import DoggiesHeader from '../components/DoggiesHeader.vue'
+
+export default {
+  name: 'TheDoggiesIndexPage',
+  components: {
+    DoggiesHeader,
+    TheDoggies,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-height: 100vh;
+  line-height: 1.5;
+  margin: 0 0 1rem 0;
+}
+
+.logo {
+  display: block;
+  height: 2.5rem;
+  width: 2.5rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 0 0.5rem 0;
+  }
+}
+</style>
