05b7566cd53d547fcc8001b1781bc040ba9a4fa3
Frank Corona Prendes <frank.corona@pm.me>
2022-11-28 17:28:30 -0300

    chore: rename application title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'thedoggies',
+    title: 'The Doggies',
     htmlAttrs: {
       lang: 'en',
     },
